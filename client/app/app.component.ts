aad71173c36546d4d0f8d57ee2064d753ed36536
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-06-21 23:48:57 -0300

    consuming api

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,20 @@
 import {Component} from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
-    selector: 'app',
-    templateUrl: './app/app.component.html'
+  moduleId: module.id,
+  selector: 'app',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  fotos: Object[] = [];
+
+  constructor(http: Http){
+    http.get('v1/fotos')
+      .map(response => response.json())
+      .subscribe(
+        fotos => this.fotos = fotos,
+        erro  => console.log(erro)
+      );
+  }
+}
