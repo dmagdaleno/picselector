f052a9ee8f110e61b01ac7d36297a022c9ee81ed
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-06-24 22:58:32 -0300

    form and form validation

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,19 +1,21 @@
 import 'rxjs/add/operator/map';
 
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AppComponent }   from './app.component';
-import { routing }   from './app.routes';
-import { FotoModule } from './foto/foto.module';
-import { PainelModule } from './painel/painel.module';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { CadastroComponent }   from './cadastro/cadastro.component';
-import { ListaComponent }   from './lista/lista.component'; 
+import { AppComponent }      from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FotoModule }        from './foto/foto.module';
+import { ListaComponent }    from './lista/lista.component'; 
+import { PainelModule }      from './painel/painel.module';
+import { routing }           from './app.routes';
 
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing ],
+  imports: [ BrowserModule, FotoModule, HttpModule, PainelModule, routing, FormsModule,
+             ReactiveFormsModule ],
   declarations: [ AppComponent, CadastroComponent, ListaComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'painel',
     templateUrl: './painel.component.html'
 })
-export class PainelComponent {
+export class PainelComponent implements OnInit {
 
     @Input() titulo: string;
+
+    ngOnInit(){
+        this.titulo = this.compressBigTitles(this.titulo, 7);
+    }
+
+    compressBigTitles(title: string, maxSize: number): string{
+        return title.length > maxSize ? title.substr(0,maxSize).concat('...') : title;
+    }
 }
